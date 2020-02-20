11f14a96dda5957b1737bb333aaf54999e61992d
Luigi Cruz <luigifcruz@gmail.com>
2020-02-19 22:19:40 -0300

    Add custom wallpaper.

--- builder/stage4/04-pisdr-system/01-run.sh
+++ builder/stage4/04-pisdr-system/01-run.sh
@@ -1,3 +1,9 @@
 install -m 755 shortcut/gnuradio.desktop "/home/${FIRST_USER_NAME}/Desktop"
 install -m 755 shortcut/gqrx.desktop "/home/${FIRST_USER_NAME}/Desktop"
 install -m 755 shortcut/sdrangel.desktop "/home/${FIRST_USER_NAME}/Desktop"
+
+install -m 755 wallpaper/donald-giannatti-vla.jpg "${ROOTFS_DIR}/usr/share/rpd-wallpaper/"
+
+on_chroot << EOF
+pcmanfm --set-wallpaper ${ROOTFS_DIR}/usr/share/rpd-wallpaper/donald-giannatti-vla.jpg
+EOF
