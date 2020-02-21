c37105a2dfadf8380c630841b7cb76538009ebba
Luigi Cruz <luigifcruz@gmail.com>
2020-02-21 19:48:46 -0300

    Enable custom wallpaper.

--- builder/stage4/04-pisdr-system/01-run.sh
+++ builder/stage4/04-pisdr-system/01-run.sh
@@ -3,3 +3,8 @@ install -m 755 shortcut/gqrx.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desk
 install -m 755 shortcut/sdrangel.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop"
 
 install -m 755 wallpaper/donald-giannatti-vla.jpg "${ROOTFS_DIR}/usr/share/rpd-wallpaper/"
+
+on_chroot << EOF
+export DISPLAY=:0.0
+pcmanfm --set-wallpaper ${ROOTFS_DIR}/usr/share/rpd-wallpaper/donald-giannatti-vla.jpg
+EOF
