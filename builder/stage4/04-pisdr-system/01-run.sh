f97b95c205aa18bed5a006559264e18bc2288e4f
Luigi Freitas Cruz <luigifcruz@me.com>
2020-02-22 03:48:08 -0300

    Fix wallpaper setting.

--- builder/stage4/04-pisdr-system/01-run.sh
+++ builder/stage4/04-pisdr-system/01-run.sh
@@ -5,6 +5,5 @@ install -m 755 shortcut/sdrangel.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/
 install -m 755 wallpaper/donald-giannatti-vla.jpg "${ROOTFS_DIR}/usr/share/rpd-wallpaper/"
 
 on_chroot << EOF
-export DISPLAY=:0.0
-pcmanfm --set-wallpaper ${ROOTFS_DIR}/usr/share/rpd-wallpaper/donald-giannatti-vla.jpg
+env DISPLAY=:0.0 pcmanfm --set-wallpaper ${ROOTFS_DIR}/usr/share/rpd-wallpaper/donald-giannatti-vla.jpg
 EOF
