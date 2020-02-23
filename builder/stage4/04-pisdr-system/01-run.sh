d1be5fb5667798ebff44f85cbde26f0d9016c7d4
Luigi Freitas Cruz <luigifcruz@me.com>
2020-02-23 14:29:21 -0300

    Automatic Wallpaper.

--- builder/stage4/04-pisdr-system/01-run.sh
+++ builder/stage4/04-pisdr-system/01-run.sh
@@ -3,3 +3,8 @@ install -m 755 shortcut/gqrx.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desk
 install -m 755 shortcut/sdrangel.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop"
 
 install -m 755 wallpaper/donald-giannatti-vla.jpg "${ROOTFS_DIR}/usr/share/rpd-wallpaper/"
+
+on_chroot << EOF
+rm -fr /usr/share/rpd-wallpaper/road.jpg
+mv /usr/share/rpd-wallpaper/donald-giannatti-vla.jpg /usr/share/rpd-wallpaper/road.jpg 
+EOF
