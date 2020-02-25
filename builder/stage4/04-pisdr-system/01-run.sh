0a649a4fbd45af09b73f805ebd1798263433e1a8
Luigi Freitas Cruz <luigifcruz@me.com>
2020-02-25 15:24:30 -0300

    Fix automatic wallpaper.

--- builder/stage4/04-pisdr-system/01-run.sh
+++ builder/stage4/04-pisdr-system/01-run.sh
@@ -3,7 +3,8 @@ install -D -m 755 shortcut/gnuradio.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAM
 install -D -m 755 shortcut/gqrx.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop"
 install -D -m 755 shortcut/sdrangel.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop"
 
-install -D -m 755 wallpaper/donald-giannatti-vla.jpg "${ROOTFS_DIR}/usr/share/rpd-wallpaper/"
+install -d "${ROOTFS_DIR}/usr/share/rpd-wallpaper"
+install -D -m 755 wallpaper/donald-giannatti-vla.jpg "${ROOTFS_DIR}/usr/share/rpd-wallpaper"
 
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/menus/"
