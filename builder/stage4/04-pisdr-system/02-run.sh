0321d77e0076bb75df7bf9611b6b335ea72259bf
Luigi Freitas Cruz <luigifcruz@me.com>
2020-02-25 16:10:01 -0300

    Add PiSDR Desktop Link.

--- builder/stage4/04-pisdr-system/02-run.sh
+++ builder/stage4/04-pisdr-system/02-run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
 on_chroot << EOF
-mkdir -p 	"/home/${FIRST_USER_NAME}/PiSDR"
-mkdir -p 	"/home/${FIRST_USER_NAME}/PiSDR/Software"
-mkdir -p 	"/home/${FIRST_USER_NAME}/PiSDR/Radios"
+mkdir -p "/home/${FIRST_USER_NAME}/PiSDR"
+ln -sf "/home/${FIRST_USER_NAME}/PiSDR" PiSDR
+
+mkdir -p "/home/${FIRST_USER_NAME}/PiSDR/Software"
+mkdir -p "/home/${FIRST_USER_NAME}/PiSDR/Radios"
 EOF
