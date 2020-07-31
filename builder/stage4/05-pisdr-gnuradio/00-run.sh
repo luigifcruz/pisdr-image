6223f86897eae835caf910870d2c5a48057a2279
Luigi Cruz <luigifcruz@gmail.com>
2020-07-31 01:46:13 -0300

    Fix GNURadio Build

--- builder/stage4/05-pisdr-gnuradio/00-run.sh
+++ builder/stage4/05-pisdr-gnuradio/00-run.sh
@@ -4,6 +4,22 @@ on_chroot << EOF
 mkdir -p 	"/home/${FIRST_USER_NAME}/PiSDR/Software"
 cd 		    "/home/${FIRST_USER_NAME}/PiSDR/Software"
 
+if [ ! -d "volk" ]; then
+    git clone --recursive https://github.com/gnuradio/volk.git
+fi
+
+cd volk
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ../
+make -j$(nproc) install
+ldconfig
+EOF
+
+on_chroot << EOF
+mkdir -p 	"/home/${FIRST_USER_NAME}/PiSDR/Software"
+cd 		    "/home/${FIRST_USER_NAME}/PiSDR/Software"
+
 if [ ! -d "gnuradio" ]; then
     git clone --recursive https://github.com/gnuradio/gnuradio.git
 fi
@@ -12,7 +28,8 @@ cd gnuradio
 git checkout maint-3.7
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python ../
+cmake -DENABLE_INTERNAL_VOLK=OFF -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python ../
 make -j$(nproc) install
 ldconfig
 EOF
+
