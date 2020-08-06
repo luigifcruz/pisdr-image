7cae172129a8450f6d854a154589dd78254c785e
Luigi Cruz <luigifcruz@gmail.com>
2020-08-06 14:45:45 -0300

    Fix env inside bashrc.

--- builder/stage4/06-pisdr-gnuradio/00-run.sh
+++ builder/stage4/06-pisdr-gnuradio/00-run.sh
@@ -13,7 +13,7 @@ git checkout maint-3.8
 git submodule update --init --recursive
 mkdir -p build
 cd build
-echo 'export PYTHONPATH=/usr/local/lib/python3/dist-packages:$PYTHONPATH' >> /home/${FIRST_USER_NAME}/.bashrc
+echo "export PYTHONPATH=/usr/local/lib/python3/dist-packages:$PYTHONPATH" >> /home/${FIRST_USER_NAME}/.bashrc
 cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python3 ..
 ninja install
 ldconfig
