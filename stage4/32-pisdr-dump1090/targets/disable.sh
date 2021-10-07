39136d2dc85adf007619e8560e9df78b9957ece1
Luigi Cruz <luigifcruz@gmail.com>
2021-10-07 16:46:10 -0300

    remove dump1090

--- stage4/32-pisdr-dump1090/targets/disable.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-systemctl disable --now readsb
-systemctl disable --now tar1090
-systemctl disable --now lighttpd
--- stage4/32-pisdr-dump1090/targets/enable.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-systemctl enable --now readsb
-systemctl enable --now tar1090
-systemctl enable --now lighttpd
