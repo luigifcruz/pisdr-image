faef69cbc731a8d6dc812098bda39c08761e2f5b
Luigi Cruz <luigifcruz@gmail.com>
2021-10-07 20:13:35 -0300

    update foxtelem

--- stage4/32-pisdr-foxtelem/00-run.sh
+++ stage4/32-pisdr-foxtelem/00-run.sh
@@ -5,7 +5,7 @@ mkdir -p    "/home/${FIRST_USER_NAME}/PiSDR/Software"
 cd 		    "/home/${FIRST_USER_NAME}/PiSDR/Software"
 
 if [ ! -d "FoxTelem" ]; then
-    wget http://amsat.us/FoxTelem/linux/FoxTelem_1.08z5_linux.tar.gz
+    wget http://amsat.us/FoxTelem/linux/FoxTelem_1.11g2_linux.tar.gz
     tar -xvzf  FoxTelem_1.08z5_linux.tar.gz
     mv FoxTelem_1.08z5_linux FoxTelem
 fi
