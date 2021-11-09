5b977b7c8efcccc17af88281f1a24eb7566d44a4
Luigi Cruz <luigifcruz@gmail.com>
2021-11-09 15:35:34 -0300

    revert arch detection

--- build-docker.sh
+++ build-docker.sh
@@ -78,7 +78,14 @@ BUILD_OPTS="$(echo "${BUILD_OPTS:-}" | sed -E 's@\-c\s?([^ ]+)@-c /config@')"
 # Check the arch of the machine we're running on. If it's 64-bit, use a 32-bit base image instead
 case "$(uname -m)" in
   x86_64|aarch64)
-    BASE_IMAGE=arm32v7/debian:bullseye
+    case "$(uname -m)" in
+        aarch64)
+            BASE_IMAGE=arm32v7/debian:bullseye
+            ;;
+        *)
+            BASE_IMAGE=i386/debian:bullseye
+            ;;
+    esac
     ;;
   *)
     BASE_IMAGE=debian:bullseye
