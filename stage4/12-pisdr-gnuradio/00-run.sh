56d915fcf8c7fc10540da5e6a34e0cf3ed31d53c
luigifcruz <luigifcruz@gmail.com>
2021-12-17 20:57:21 -0300

    patch cpu_features for arm64

--- stage4/12-pisdr-gnuradio/00-run.sh
+++ stage4/12-pisdr-gnuradio/00-run.sh
@@ -5,13 +5,17 @@ mkdir -p 	"/home/${FIRST_USER_NAME}/PiSDR/Software"
 cd 		    "/home/${FIRST_USER_NAME}/PiSDR/Software"
 
 if [ ! -d "volk" ]; then
-	git clone --single-branch --branch v2.5.0 --depth 1 --recurse-submodules --shallow-submodules https://github.com/gnuradio/volk.git
+	git clone --single-branch --branch v2.5.0 --depth 1 --recurse-submodules --shallow-submodules https://github.com/luigifcruz/volk.git
 fi
 
 cd volk
+git checkout m1fix
 mkdir -p build
 cd build
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_CXX_FLAGS:STRING="-march=armv8-a -Wno-psabi" \
+      -DCMAKE_C_FLAGS:STRING="-march=armv8-a -Wno-psabi" \
+	  DCMAKE_ASM_FLAGS:STRING="-march=armv8-a -Wno-psabi" \
+	  -GNinja -DCMAKE_BUILD_TYPE=Release ..
 ninja install
 ldconfig
 cd ..
