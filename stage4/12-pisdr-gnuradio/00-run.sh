84786599083bceb076b258d21cf3d7b35b969b26
luigifcruz <luigifcruz@gmail.com>
2021-12-18 01:18:43 -0300

    turn cpu_features off

--- stage4/12-pisdr-gnuradio/00-run.sh
+++ stage4/12-pisdr-gnuradio/00-run.sh
@@ -5,13 +5,16 @@ mkdir -p 	"/home/${FIRST_USER_NAME}/PiSDR/Software"
 cd 		    "/home/${FIRST_USER_NAME}/PiSDR/Software"
 
 if [ ! -d "volk" ]; then
-	git clone --single-branch --depth 1 --recurse-submodules --shallow-submodules https://github.com/luigifcruz/volk.git
+	git clone --single-branch --branch v2.5.0 --depth 1 --recurse-submodules --shallow-submodules https://github.com/gnuradio/volk.git
 fi
 
 cd volk
 mkdir -p build
 cd build
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_CXX_FLAGS:STRING="-march=armv8-a -Wno-psabi" \
+      -DCMAKE_C_FLAGS:STRING="-march=armv8-a -Wno-psabi" \
+	  DCMAKE_ASM_FLAGS:STRING="-march=armv8-a -Wno-psabi" \
+	  -GNinja -DVOLK_CPU_FEATURES=OFF -DCMAKE_BUILD_TYPE=Release ..
 ninja install
 ldconfig
 cd ..
