6abdc6ae3a72bc6da20b9c4fee53556de6aba83d
luigifcruz <luigifcruz@gmail.com>
2021-12-24 17:57:59 -0300

    Second batch of new applications for v6.0.

---
+++ stage4/37-pisdr-nrsc5/00-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+on_chroot << EOF
+mkdir -p    "/home/${FIRST_USER_NAME}/PiSDR/Software"
+cd 		    "/home/${FIRST_USER_NAME}/PiSDR/Software"
+
+if [ ! -d "nrsc5" ]; then
+    git clone --depth 1 https://github.com/theori-io/nrsc5.git
+fi
+
+cd nrsc5
+mkdir -p build
+cd build
+cmake -DUSE_NEON=OFF -DUSE_FAAD2=OFF -GNinja ..
+ninja install
+ldconfig
+cd ..
+rm -fr build
+EOF
