10f9cd715cd44131b0b5b25fad9bafa4e3ed533d
luigifcruz <luigifcruz@gmail.com>
2021-12-24 19:52:09 -0300

    Fix SDRplay build script.

--- stage4/07-pisdr-sdrplay/00-run.sh
+++ stage4/07-pisdr-sdrplay/00-run.sh
@@ -13,7 +13,7 @@ chmod +x ./SDRplay_RSP_API-ARM32-3.07.2.run
 ./SDRplay_RSP_API-ARM32-3.07.2.run --check
 ./SDRplay_RSP_API-ARM32-3.07.2.run --noexec --target .
 sed -i 's/more sdrplay_license.txt//g' install_lib.sh
-sed -i 's/armv7l/armv8l/g' install_lib.sh
+sed -i 's/ARCH=`uname -m`/ARCH=armv7l/g' install_lib.sh
 printf "\ny\ny\ny\n" | ./install_lib.sh
 systemctl enable sdrplay
 ldconfig
