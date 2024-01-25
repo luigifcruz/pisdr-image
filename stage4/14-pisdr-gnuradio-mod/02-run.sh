4e10ddb3e36ab2c9b78b59ef2e47d879a4639269
n1ai <dcherkus@yahoo.com>
2024-01-25 18:21:38 -0500

    n1ai build fixes

--- stage4/14-pisdr-gnuradio-mod/02-run.sh
+++ stage4/14-pisdr-gnuradio-mod/02-run.sh
@@ -5,7 +5,7 @@ mkdir -p    "/home/${FIRST_USER_NAME}/PiSDR/Software"
 cd 		    "/home/${FIRST_USER_NAME}/PiSDR/Software"
 
 if [ ! -d "gr-osmosdr" ]; then
-    git clone --single-branch --branch master --depth 1 git://git.osmocom.org/gr-osmosdr
+    git clone --single-branch --branch master --depth 1 https://github.com/osmocom/gr-osmosdr.git
 fi
 
 cd gr-osmosdr
--- stage4/32-pisdr-cyberether/00-run.sh
+++ stage4/32-pisdr-cyberether/00-run.sh
@@ -5,7 +5,7 @@ mkdir -p    "/home/${FIRST_USER_NAME}/PiSDR/Software"
 cd 		    "/home/${FIRST_USER_NAME}/PiSDR/Software"
 
 if [ ! -d "CyberEther" ]; then
-    git clone --branch development --depth 1 https://github.com/luigifcruz/CyberEther.git
+    git clone --depth 1 https://github.com/luigifcruz/CyberEther.git
     cd CyberEther
     meson -Dbuildtype=debugoptimized build
     cd build
